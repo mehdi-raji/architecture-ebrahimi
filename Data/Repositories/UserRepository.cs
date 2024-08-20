01a4cbb782eb080e6bba2f7e56a0a68706a1de44
Mehdi Raji <148987332+mehdi-raji@users.noreply.github.com>
2024-08-21 00:10:00 +0330

    RepositoryVideoImplemented

---
+++ Data/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Common.Utilities;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Data.Repositories
+{
+	public class UserRepository : Repository<User>
+	{
+		public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+		{
+		}
+		public Task<User> GetByUserAndPass(string userName,string password,CancellationToken cancellationToken)
+		{
+			var passwordHash = SecurityHelper.GetSha256Hash(password);
+			return Table.Where(p => p.UserName == userName && p.PasswordHash == passwordHash).SingleOrDefaultAsync(cancellationToken);
+		}
+	}
+}
